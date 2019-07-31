4e7da4d42f97608f5b9433e2932bafb9e34f6d0c
Matthew Jones <matthew.jones@stonybrook.edu>
2019-07-31 13:58:12 -0400

    Location Services are now prompted to be on

--- app/src/main/java/com/example/weatherprepared/MainActivity.java
+++ app/src/main/java/com/example/weatherprepared/MainActivity.java
@@ -2,33 +2,33 @@ package com.example.weatherprepared;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
-    //final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showLocationAlert();
 
-        //if (!isLocationEnabled()){
-          //  showLocationAlert();
-        //}
+        if (isLocationEnabled()){
+            showLocationAlert();
+        }
     }
 
-    //private boolean isLocationEnabled(){
-        //return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    //}
+    private boolean isLocationEnabled(){
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+    }
 
     private void showLocationAlert(){
         final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
